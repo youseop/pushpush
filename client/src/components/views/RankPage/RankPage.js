ce3a46a8faf4cb23210a5d08629fd201eaacc99e
youseop <edlsw@naver.com>
2021-05-22 05:15:21 +0900

    rank api

--- client/src/components/views/RankPage/RankPage.js
+++ client/src/components/views/RankPage/RankPage.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import { useConstructor } from '../../functions/useConstructor'
 
 function RankPage() {
